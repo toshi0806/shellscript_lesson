4447ab0634062815637371f313b285a822848fec
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2021-05-14 10:48:41 +0900

    インデントを修正
    ブロックの区切りは別の処理のあとに続けて書かないほうが良い
    ・具体的には if, else, fi, while done など
    ・then, do などは、if や while と組でブロックの開始を示すので、それら
    　と同一行でも問題ない
    　・shell script は文法が特殊なので、この辺は分かりにくい。
    　・一般的なプログラミング言語で勉強したほうが良いよ
    
    インデントを正しくすることで、制御構造が視覚的にも理解しやすくなる

--- converter.sh
+++ converter.sh
@@ -4,19 +4,22 @@
 while(true);do
     read -p "original path : " filePass
     if [ "$filePass" != "" ]; then
-        break;fi
-    done
+        break;
+    fi
+done
 
 read -p "converted path : " convertname
-    if [ "$convertname" != "" ]; then
+if [ "$convertname" != "" ]; then
     convertname=`echo ${convertname}.mcfunction`
     filedir=`dirname ${filePass}`
     filedir=`echo ${filedir}\'`
     cd "${filedir}"
-    touch ${convertname}; else
-    convertname='converted.mcfunction' ;fi
+    touch ${convertname};
+else
+    convertname='converted.mcfunction'
+fi
 
-    echo "###########"
+echo "###########"
 
 # 動作チェック用に追加
 echo "From: $filePass"
