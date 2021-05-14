64f5bf1336cdf2eb87c2e05a6c14bbf7ae61e376
Toshihiko SHIMOKAWA <toshi@smkwlab.net>
2021-05-14 10:46:31 +0900

    initial commit

---
+++ converter.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# 某所から拾ってきた shell script の断片
+while(true);do
+    read -p "original path : " filePass
+    if [ "$filePass" != "" ]; then
+        break;fi
+    done
+
+read -p "converted path : " convertname
+    if [ "$convertname" != "" ]; then
+    convertname=`echo ${convertname}.mcfunction`
+    filedir=`dirname ${filePass}`
+    filedir=`echo ${filedir}\'`
+    cd "${filedir}"
+    touch ${convertname}; else
+    convertname='converted.mcfunction' ;fi
+
+    echo "###########"
